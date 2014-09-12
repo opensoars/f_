b6c563eb49cc2bfd8b43942610ae4349b9997c92
opensoars <sam_1700@hotmail.com>
2014-09-12 13:57:22 +0200

    Removed obsolete test call

--- tests/dev.js
+++ tests/dev.js
@@ -2,7 +2,6 @@ var f_ = require('../index.js');
 
 var TaskList = require('./TaskList');
 
-
 var f_config = {
 	functionFlow: ['getSource', 'writeSource', 'notify'],  // REQUIRED
 	resetOnRetryAll: true,
