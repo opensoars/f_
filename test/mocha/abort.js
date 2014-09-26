73750351ebe419176fa3dd5d402229002a178ab0
opensoars <sam_1700@hotmail.com>
2014-09-26 16:45:34 +0200

    New abort structure

--- test/mocha/abort.js
+++ test/mocha/abort.js
@@ -5,35 +5,41 @@ var f_ = require(__dirname + './../../index.js'),
 
 describe('abort', function (){
 
-  it('f_ should call `onAbort`', function (done){
+  describe('#trigger abort: true', function (){
 
-    TaskList = f_.augment(TaskList, {
-      functionFlow: ['getSource', 'writeSource', 'notify'],
-      toLog: ['none']
+    it('f_ should call `onAbort`', function (done){
+
+      TaskList = f_.augment(TaskList, {
+        functionFlow: ['getSource', 'writeSource', 'notify'],
+        toLog: ['none']
+      });
+
+      var taskList = f_.setup( new TaskList({ abort: true }) );
+      taskList.onAbort = done;
+      taskList.start();
     });
 
-    var taskList = f_.setup( new TaskList({ abort: true }) );
-    taskList.onAbort = done;
-    taskList.start();
+
+    it('f_ should have set f_status to `aborted`', function (done){
+
+      TaskList = f_.augment(TaskList, {
+        functionFlow: ['getSource', 'writeSource', 'notify'],
+        toLog: ['none']
+      });
+
+      var taskList = f_.setup( new TaskList({ abort: true }) );
+
+      taskList.onAbort = function (){
+        assert.equal(this.f_status, 'aborted');
+        done();
+      };
+
+      taskList.start();
+    });
+
   });
 
 
-  it('f_ should have set f_status to `aborted`', function (done){
-
-    TaskList = f_.augment(TaskList, {
-      functionFlow: ['getSource', 'writeSource', 'notify'],
-      toLog: ['none']
-    });
-
-    var taskList = f_.setup( new TaskList({ abort: true }) );
-
-    taskList.onAbort = function (){
-      assert.equal(this.f_status, 'aborted');
-      done();
-    };
-
-    taskList.start();
-  });
 
 });
 
