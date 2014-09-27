aa1d0280e3862cc9835828321f6755211b367a26
opensoars <sam_1700@hotmail.com>
2014-09-27 20:24:48 +0200

    Small global variable fix

--- test/mocha/noOnEvents.js
+++ test/mocha/noOnEvents.js
@@ -28,7 +28,7 @@ describe('noOnEvents', function (){
   describe('#onAbort', function (){
     it('should be able to abort the task with no onAbort (using timeout to check)', function (done){
 
-      TaskList = f_.augment(TaskList, {
+      var TaskList = f_.augment(TaskList, {
         functionFlow: ['getSource', 'writeSource', 'notify']
       });
 
@@ -49,7 +49,7 @@ describe('noOnEvents', function (){
   describe('#onNext', function (){
     it('should be able to call f_next with no onNext (using timeout to check)', function (done){
 
-      TaskList = f_.augment(TaskList, {
+      var TaskList = f_.augment(TaskList, {
         functionFlow: ['getSource', 'writeSource', 'notify']
       });
 
