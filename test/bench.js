9d574683a68701f8c11719e4fb2edce5c9468df8
opensoars <sam_1700@hotmail.com>
2014-09-29 16:22:43 +0200

    Small readme typo fix

--- test/bench.js
+++ test/bench.js
@@ -1,33 +1,9 @@
-var f_ = require('./../index.js');
+var f_ = require('./../index.js'),
+		TaskList = require('./lib/TaskList.js');
 
-var TaskList = require('./lib/TaskList.js');
-
-var f_config = {
-
-	/**
-	 * `start` method is not given here, since we call it manualy
-	 * This is just a matter of personal taste, I do not like auto starts!
-	 */
-	functionFlow: ['getSource', 'writeSource', 'notify'],  // REQUIRED
-	resetOnRetryAll: true,
-
-	toLog: ['none'],
-
-	desc: 'Test taskList',
-
-	maxRetries: {
-		all: 2
-	},
-
-	maxTries: {
-		getSource: 1,
-		writeSource: 2,
-		notify: 1
-	}
-
-};
-
-TaskList = f_.augment(TaskList, f_config);
+TaskList = f_.augment(TaskList, {
+	functionFlow: ['getSource', 'writeSource', 'notify']  // REQUIRED
+});
 
 
 function runSingle(){
@@ -43,7 +19,6 @@ function runSingle(){
 		//retryThis:     true
 	});
 
-
 	taskListInstance = f_.setup(taskListInstance);
 
 	taskListInstance.start();
@@ -51,21 +26,12 @@ function runSingle(){
 
 
 function runMultiple(){
-	var startTime = Date.now(); 
+	var startTime = Date.now();
 
-	for(var i=0; i<100000; i+=1){
-		var taskListInstance = new TaskList();
-		taskListInstance = f_.setup(taskListInstance);
+	for(var i=0; i<100000; i+=1)
+		f_.setup(new TaskList()).start();
 
-		taskListInstance.f_desc = taskListInstance.f_desc + ' #' + i;
-
-		taskListInstance.start();
-	}
-
-	var endTime = Date.now(),
-			timeTaken = endTime - startTime;
-
-	console.log(timeTaken);
+	console.log();
 };
 
 
