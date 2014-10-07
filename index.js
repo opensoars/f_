4ed01a793f47c70102264fdd559bcb1971f34570
opensoars <sam_1700@hotmail.com>
2014-10-07 11:57:11 +0200

    Code/comment cleanup

--- index.js
+++ index.js
@@ -1,4 +1,24 @@
+/** f_ project structure if it would be in one file
+ * node_modules
+ * colored logging functions
+ * globals
+ * methods
+ * augment
+ * setup
+ */
+
 module.exports = {
+
+  /**
+   * DEPENDENCIES
+   * ./methods.js  Collection of all f_ methods
+   */
   augment: require('./lib/augment.js'),
+
+
+  /**
+   * DEPENDENCIES
+   * NONE
+   */
   setup: require('./lib/setup.js')
 };
