b9f2af7c7f1f928ee0e038c92f529a2e8d7240f2
unknown <sam_1700@hotmail.com>
2015-10-05 12:47:22 +0200

    New modular design

---
+++ index.js
@@ -0,0 +1,25 @@
+/**
+ * Main f_ module, this is what you get when you require('f_').
+ * @module f_
+ */
+
+/**
+ * Makes it a lot easier to use the fs in deeply nested file structures. This
+ * is value of the `__dirname` global variable available in index.js.
+ * @type {string}
+ * @GLOBAL
+ */
+process.ROOT_DIR = __dirname;
+
+/**
+ * Makes it a lot easier to require in deeply nested file structures. This is
+ * the original nodejs require function.
+ * @type {function}
+ * @GLOBAL
+ */
+process.ROOT_REQUIRE = require;
+
+
+module.exports = {
+  getConstructor: require('./lib/getConstructor')
+};
---
+++ insta.js
@@ -0,0 +1,47 @@
+var f_ = require('./index.js');
+
+
+var Download = f_.getConstructor({
+
+  initializer: function () {
+    //console.log('initializer called, this:', this);
+    //this.on('f_log', function () { console.log('log!'); });
+    this.f_log('Log from initializer');
+    this.f_err('Err from initializer');
+
+    this.f_next();
+  },
+
+  function_flow: [
+    {
+      name: 'start',
+      function: function start() {
+        console.log('start');
+        this.f_next();
+      },
+      max_tries: 3
+    },
+    {
+      name: 'method1',
+      function: function method1() {
+        console.log('method1');
+      },
+      max_tries: 2
+    },
+    {
+      name: 'method2',
+      function: function method2() {
+        console.log('method2');
+      },
+      max_tries: 1
+    }
+  ]
+});
+
+
+var dl = new Download();
+
+console.log(dl);
+
+//dl.f_err('Fail!');
+//console.log(dl);
