b34b3e411ce5d641ae22ae944881b93d63764cae
unknown <sam_1700@hotmail.com>
2015-10-06 14:12:46 +0200

    instance_modules/methods dupe fix

---
+++ tests/mocha/instance_modules.js
@@ -0,0 +1,14 @@
+var assert = require('assert');
+
+describe('instance_modules', function (){
+  var f_ = require('./../../index.js');
+
+  describe('err', function (){
+  });
+
+  describe('log', function (){
+  });
+
+  describe('history', function (){
+  });
+});
