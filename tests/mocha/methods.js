7f3f258ed4ac0298f200beccf725cec6fbc2ee8f
unknown <sam_1700@hotmail.com>
2015-10-14 12:48:34 +0200

    f_go arg pass fix + tests, abort test, fixes

---
+++ tests/mocha/methods.js
@@ -0,0 +1,64 @@
+var assert = require('assert');
+
+describe('methods', function (){
+
+  var f_ = require('./../../index.js');
+
+  describe('#abort', function (){
+    it('emits an abort event with an err_object.message prefixed with "abort, "', function (done){
+      var test = new (f_.getConstructor({
+        function_flow: []
+      }));
+
+      test.on('abort', function (err_object) {
+        assert.equal(err_object.message, 'abort, test1');
+        done();
+      });
+
+      test.f_abort('test1');
+    });
+
+    it('results in not calling any more functions from function_flow', function (done) {
+      var next_called = false;
+
+      var test = new (f_.getConstructor({
+        function_flow: [
+          { name: 'one', function: function () { this.f_abort(); } },
+          { name: 'two', function: function () { this.f_next(); } }
+        ]
+      }));
+
+      setTimeout(function () {
+        assert.equal(next_called, false);
+        done();
+      }, 50);
+
+      test.f_go();
+
+      test.on('next', function () {
+        next_called = true;
+      });
+    });
+  });
+
+  describe('#go', function (){
+    it('calls f_next with the passed arguments', function (done){
+      
+      var test = new (f_.getConstructor({
+        function_flow: [
+          { name: 'one',
+            function: function (test1, test2) {
+              assert.equal(test1, 'test1');
+              assert.equal(test2, 'test2');
+              done();
+            }
+          },
+        ]
+      }));
+
+      test.f_go('test1', 'test2');
+
+    });
+  });
+
+});
