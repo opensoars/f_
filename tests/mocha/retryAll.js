2c7c5552c9218b092bead5435884f1a00ee9e131
unknown <sam_1700@hotmail.com>
2015-11-03 12:27:31 +0100

    retryAll tests and coverage

--- tests/mocha/retryAll.js
+++ tests/mocha/retryAll.js
@@ -1,29 +1,67 @@
 var assert = require('assert');
 
 describe('retryAll', function (){
-  it('resets f_flow_i to 0 and starts the flow from the first method specified in the function_flow', function (done){
-    var f_ = require('./../../index.js');
+  var f_ = require('./../../index.js');
 
+  it('resets f_flow_i to 0 and starts the flow from the first method specified in the function_flow', function (done){
     var methods_called = 0;
 
     var Constructor = f_.getConstructor({
       function_flow: [
-        m1: function () {
-          methods_called++;
-          this.f_next();
+        {
+          name: 'm1',
+          function: function () {
+            methods_called++;
+            this.f_next();
+          },
+          max_tries: 5
         },
-        m2: function () {
-          methods_called++;
-          this.f_next();
-        },
-        m3: function () {
-          methods_called++;
-          this.f_next();
+        {
+          name: 'm2',
+          function: function () {
+            methods_called++;
+            this.f_retryAll();
+          },
+          max_tries: 5
         }
       ]
     });
 
-    (new Constructor()).go();
+    var instance = new Constructor();
+
+    instance.f_go();
+    
+    assert.equal(methods_called, 10);
+    done();
+  });
+
+  it('emits an retryAll event', function (done) {
+    var Constructor = f_.getConstructor({
+      function_flow: [
+        {
+          name: 'm1',
+          function: function () {
+            this.f_next();
+          },
+          max_tries: 1
+        },
+        {
+          name: 'm2',
+          function: function () {
+            this.f_retryAll();
+          },
+          max_tries: 1
+        }
+      ]
+    });
+
+    var instance = new Constructor();
+
+    instance.on('retryAll', function () {
+      done();
+    });
+
+    instance.f_go();
 
   });
 });
